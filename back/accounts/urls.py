327552ad27bcf7b11d26a7d6859d8172aaf89297
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-07-17 18:50:23 -0400

    feat: authentication

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt import views  # new
+from rest_framework_simplejwt import views
 from .views import AccountsView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path('token/', views.TokenObtainPairView.as_view()),  # new
-    path('token/refresh/', views.TokenRefreshView.as_view()),  # new
+    path('token/', views.TokenObtainPairView.as_view()),
+    path('token/refresh/', views.TokenRefreshView.as_view()),
     path('accounts/', AccountsView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
 ]
